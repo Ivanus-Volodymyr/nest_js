11072d756bfdc9616317f5c63c215fe8d3dedbbb
ivanys23071996 <ivanys230796@gmail.com>
2022-06-07 22:41:05 +0300

    users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-import { PrismaService } from "../core";
+import { PrismaService } from '../core';
 import { Prisma, User } from '@prisma/client';
 
 @Injectable()
