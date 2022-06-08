c9ee5bf07d59b4f4050fe8273c6ef04e4c832aa1
ivanys23071996 <ivanys230796@gmail.com>
2022-06-08 14:29:48 +0300

    users

--- src/users/dto/create-user-dto.ts
+++ src/users/dto/create-user-dto.ts
@@ -1,12 +1,40 @@
-import { CreatePostsDto } from '../../posts/dto/create-posts-dto';
+import { CreatePostsDto } from '../../posts/dto';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+} from 'class-validator';
+import { CommentCreateDto } from '../../comments/dto';
 
 export class CreateUserDto {
+  @IsNumber()
   id: number;
+
+  @IsString()
+  @Length(2, 10)
+  @IsNotEmpty()
   public name: string;
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   public email: string;
+
+  @IsNumber()
   public age: number;
+
+  @IsString()
+  @Length(2, 10)
   public city: string;
+
+  @IsBoolean()
   public status: boolean;
+  @IsString()
+  @Length(2, 20)
+  @IsNotEmpty()
   readonly password: string;
   public posts?: CreatePostsDto[];
+  public comments?: CommentCreateDto[];
 }
--- src/users/dto/update-user-dto.ts
+++ src/users/dto/update-user-dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty, IsNumber, IsString, Length } from 'class-validator';
+
 export class UpdateUserDto {
+  @IsString()
+  @Length(2, 10)
+  @IsNotEmpty()
   public name: string;
+  @IsNumber()
   public age: number;
+  @IsString()
   public city: string;
 }
