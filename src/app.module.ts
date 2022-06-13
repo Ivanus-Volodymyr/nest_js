d52926cb71f9ad2285083a1313b605d34775a313
ivanys23071996 <ivanys230796@gmail.com>
2022-06-13 11:01:01 +0300

    app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, CommentsModule],
+  imports: [UsersModule, PostsModule, CommentsModule, AuthModule],
 })
 export class AppModule {}
