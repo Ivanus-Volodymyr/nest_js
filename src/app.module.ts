a903661ac52fc15957737c5af528bea30b7ee31c
ivanys23071996 <ivanys230796@gmail.com>
2022-06-16 16:28:53 +0300

    app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { AuthModule } from './auth/auth.module';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, CommentsModule, AuthModule],
+  imports: [UsersModule, PostsModule, CommentsModule, AuthModule, FileModule],
 })
 export class AppModule {}
